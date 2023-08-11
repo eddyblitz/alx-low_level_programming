acc09b3950cdc711078240e828d884e196576d49
Edwin debrah <edwindebrah000@gmail.com>
2023-08-11 11:37:36 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- * main - C program that prints the size of various programs
+ * main - Entry point
  *
- * Return: always (0)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
-}
+{
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
