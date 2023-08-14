898f3046fc4b8a73b2397a59808a74ac2bdf447e
Edwin debrah <edwindebrah000@gmail.com>
2023-08-14 12:13:22 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Always 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+
+char be;
+
+for (be = 'a'; be <= 'z'; be++)
+putchar(be);
+putchar('\n');
+
+return (0);
+}
