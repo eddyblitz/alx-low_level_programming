1718e1f835f4039927705a594327180ff1bedf64
Edwin debrah <edwindebrah000@gmail.com>
2023-08-14 16:30:02 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+int b;
+
+for (b = 0; b <= 10; b++)
+{	
+putchar(b + '0');
+
+if (b != 9)
+{	
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
