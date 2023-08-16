20b5831dfe9652edd93b8ea277f1a8b2038fbf5a
Edwin debrah <edwindebrah000@gmail.com>
2023-08-16 16:50:09 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+long long int fibo1 = 1, fibo2 = 2, next;
+
+printf("%llu, %llu", fibo1, fibo2);
+
+for (n = 2; n < 50; n++)
+{
+next = fibo1 + fibo2;
+printf(", %llu", next);
+fibo1 = fibo2;
+fibo2 = next;
+}
+
+printf("\n");
+return (0);
+}
