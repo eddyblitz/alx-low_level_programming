4e9efd8f4bddd7778453d5bcea88fd89069faba4
Edwin debrah <edwindebrah000@gmail.com>
2023-08-16 17:39:11 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -16,16 +16,13 @@ fib[1] = 2;
 while (fib[0] + fib[1] <= 4000000)
 {
 fib[2] = fib[0] + fib[1];
-        
 if (fib[2] % 2 == 0)
 {
 sum += fib[2];
 }
-        
 fib[0] = fib[1];
 fib[1] = fib[2];
 }
-
 printf("%lu\n", sum);
 
 return (0);
