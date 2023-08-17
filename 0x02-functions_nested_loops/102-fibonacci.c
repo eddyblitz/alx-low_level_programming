33e29ed7e855725e434712e71af051ae6e162d2f
Edwin debrah <edwindebrah000@gmail.com>
2023-08-16 17:18:06 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,12 +10,12 @@ int main(void)
 int n;
 unsigned long int fibo1 = 1, fibo2 = 2, next;
 
-printf("%llu, %llu", fibo1, fibo2);
+printf("%lu, " fibo1);
 
 for (n = 2; n < 50; n++)
 {
+printf("%lu", fibo2);	
 next = fibo1 + fibo2;
-printf(", %llu", next);
 fibo1 = fibo2;
 fibo2 = next;
 if (n !=49)
