a3ccb35f24a050657a9dffae85de78989f160e1e
Edwin debrah <edwindebrah000@gmail.com>
2023-08-16 17:45:00 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+unsigned long fib1 = 1;
+unsigned long fib2 = 2;
+unsigned long fib3;
+int count;
+
+printf("%lu, %lu", fib1, fib2);
+
+for (count = 3; count <= 98; count++)
+{
+fib3 = fib1 + fib2;
+printf(", %lu", fib3);
+
+if (fib3 / fib2 == 1)
+break;
+
+fib1 = fib2;
+fib2 = fib3;
+}
+
+printf("\n");
+
+return (0);
+}
