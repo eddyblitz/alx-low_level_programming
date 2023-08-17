f0397b186a242bff557f3c6d73c04b1eb4d641ad
Edwin debrah <edwindebrah000@gmail.com>
2023-08-16 17:28:56 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+unsigned long fib[3];
+unsigned long sum = 2;  // We start with 2 since the second term is even
+int i;
+
+fib[0] = 1;
+fib[1] = 2;
+
+while (fib[0] + fib[1] <= 4000000)
+{
+fib[2] = fib[0] + fib[1];
+        
+if (fib[2] % 2 == 0)
+{
+sum += fib[2];
+}
+        
+fib[0] = fib[1];
+fib[1] = fib[2];
+}
+
+printf("%lu\n", sum);
+
+return (0);
+}
